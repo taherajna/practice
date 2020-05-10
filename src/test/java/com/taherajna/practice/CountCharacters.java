f59f01894cda6c40b82e291f847404c9a0491949
taherajna <Tmnagitb786!>
2020-05-10 15:19:09 +0530

    Count characters

---
+++ src/test/java/com/taherajna/practice/CountCharacters.java
@@ -0,0 +1,31 @@
+package com.taherajna.practice;
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CountCharacters {
+
+  @Test
+  public void app() throws IOException {
+    String string = "Taher@34aT";
+    int[] expected = {2, 5, 2, 1};
+    Assert.assertArrayEquals(expected, countCharacters(string));
+  }
+
+  private int[] countCharacters(String inputString) {
+    int[] output = new int[4];
+    for (char c : inputString.toCharArray()) {
+      if (c >= 'A' && c <= 'Z') {
+        output[0]++;
+      } else if (c >= 'a' && c <= 'z') {
+        output[1]++;
+      } else if (c >= '0' && c <= '9') {
+        output[2]++;
+      } else {
+        output[3]++;
+      }
+    }
+    return output;
+  }
+}
